73900461a5ac8a0d90531a087b1a63aed13ce697
GipsyVN <hahaja1029@gmail.com>
2023-07-18 15:38:24 +0700

    final

---
+++ Data_Mahasiswa/src/data_mahasiswa/RowSelect.java
@@ -0,0 +1,17 @@
+package data_mahasiswa;
+
+/**
+ *
+ * @author Marcelius Tri Alvino
+ */
+public class RowSelect {
+    public static String no;
+    
+    public static String get_no(){
+        return no;
+    }
+    
+    public static void set_no(String no){
+    RowSelect.no = no;
+    }
+}
