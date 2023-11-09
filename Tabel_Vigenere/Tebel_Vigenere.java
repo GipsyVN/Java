201d9b9754a0c03148a37d79cf95b798dc6e4532
GipsyVN <120303120+GipsyVN@users.noreply.github.com>
2023-11-08 19:34:18 -0800

    Final

--- Tabel_Vigenere/Tebel_Vigenere.java
+++ Tabel_Vigenere/Tebel_Vigenere.java
@@ -1,22 +1,12 @@
+import java.util.Arrays;
+
 public class Tebel_Vigenere {
     public static void main(String[] args) {
         char table[][] = create();
-        
-        // for(int i = 0; i < 26; i++){
-        //     for(int j = 0; j < 26; j++){
-        //         System.out.println(Arrays.toString(table[i]));
-        //     }
-        //     System.out.println();
-        // }
-
-        for (char[] baris : table) {
-            System.out.print("{");
-            for (char col : baris) {
-                System.out.print((col + ","));
-            }
-            System.out.print("}\n");
+          
+        for(int i = 0; i < table.length; i++){
+            System.out.println(Arrays.toString(table[i]));
         }
-        //System.out.println("}\n");
     }
 
     private static char[][] create(){
